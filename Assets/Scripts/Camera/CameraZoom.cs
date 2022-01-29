90d8a8680bfd4cc05a2ee2105b79d3f381321c7b
varoGraf <grafia.perez.alvaro@gmail.com>
2022-01-29 17:13:10 +0100

    Zoom completed

---
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public Transform Token;
+    public float duration = 3.0f;
+
+    private Vector3 initialToken;
+    private Vector3 target, previousTarget;
+    private float elapsed = 0.0f;
+    private bool lookingAt = false;
+    private bool zoomed = false;
+    private float targetSize = 5f;
+    [SerializeField]
+    private float minSize = 1f;
+    [SerializeField]
+    private float maxSize = 5f;
+
+    void Start()
+    {
+        initialToken = Token.position;
+        target = initialToken;
+    }
+
+
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (zoomed)
+        {
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                target = initialToken;
+                targetSize = maxSize;
+                elapsed = 0;
+                zoomed = false;
+            }
+            if (Input.GetAxis("Fire2") > 0)
+            {
+                if (Token.gameObject.activeSelf) { target = Token.position; }
+            }
+
+        }
+        else
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                if (Token.gameObject.activeSelf) { target = Token.position; }
+                targetSize = minSize;
+                elapsed = 0;
+                zoomed = true;
+            }
+        }
+        if (previousTarget != target && !lookingAt)
+        {
+            StartCoroutine(lookAtLerp(previousTarget, target));
+            previousTarget = target;
+        }
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetSize, elapsed / duration);
+    }
+
+    IEnumerator lookAtLerp(Vector3 prev, Vector3 targ)
+    {
+        lookingAt = true;
+        float elapsedTime = 0f;
+        float durationLerp = 0.5f;
+        while (elapsedTime < durationLerp)
+        {
+            Camera.main.transform.LookAt(Vector3.Lerp(prev, targ, elapsedTime / durationLerp));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        lookingAt = false;
+    }
+}

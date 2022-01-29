8d4f43280d31d79d853dfe52911b83ecd4360d6f
Javier <mrvizious@gmail.com>
2022-01-29 17:09:45 +0100

    Revert "Merge branch 'Text_on_Tiles'"
    This reverts commit a9b7179205bf2fe1c3444bfbfcfa1549fed9f252, reversing
    changes made to 042f4428a55309b76c7be6e3ef1e6f03a25d045d.

--- Assets/Scripts/Tiles/TileLogicController.cs
+++ Assets/Scripts/Tiles/TileLogicController.cs
@@ -1,56 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+using UnityEngine.UI;
 
 public class TileLogicController : MonoBehaviour
 {
     public bool isLeft = false;
-    public int value = 1;
-    public List<TextMeshPro> numberTexts;
+    public float value = 1f;
+    public List<Text> numberTexts;
 
-    private void Start() {
-        SetValue(1);
-    }
-
-    public void MultiplyValueBy(int factor) {
+    public void MultiplyValueBy(float factor) {
         SetValue(value * factor);
     }
 
-    public void SetValue(int value) {
+    public void SetValue(float value) {
         this.value = value;
-        UpdateValueTexts();
+
     }
 
-    public void UpdateValueTexts() {
-        foreach (TextMeshPro text in numberTexts)
-        {
+    private void UpdateValueTexts() {
+        foreach (Text text in numberTexts) {
             text.text = value.ToString();
         }
     }
 }
-
-// Custom inspector with a button to multiply the value by 2
-#if UNITY_EDITOR
-[CustomEditor(typeof(TileLogicController))]
-public class TileLogicControllerEditor : Editor
-{
-    public override void OnInspectorGUI() {
-        base.OnInspectorGUI();
-
-        TileLogicController tileLogicController = (TileLogicController)target;
-
-        if (GUILayout.Button("Update Value"))
-        {
-            tileLogicController.UpdateValueTexts();
-        }
-        if (GUILayout.Button("Multiply by 2"))
-        {
-            tileLogicController.MultiplyValueBy(2);
-        }
-    }
-}
-#endif

4bdb924db6773cded497c4bc6296291e0ca0a6a0
Javier <mrvizious@gmail.com>
2022-01-30 01:48:50 +0100

    Tile references are now kept

--- Assets/Scripts/Tiles/GenerateGrid.cs
+++ Assets/Scripts/Tiles/GenerateGrid.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using System.Linq;
+
 
 public class GenerateGrid : MonoBehaviour
 {
@@ -10,40 +12,42 @@ public class GenerateGrid : MonoBehaviour
     public int PerlinRange;
     public float PerlinScale;
     float[,] P;
-    GameObject[,] TilesGrid;
-    void Start()
-    {
-        float GridOffset = (float)GridSize/2.0f;
+    public GameObject[,] TilesGrid;
+    void Start() {
+        float GridOffset = (float)GridSize / 2.0f;
         GeneratePerlinGrid(GridSize);
-        for(int i = 0; i < GridSize; i++)
+        TilesGrid = new GameObject[GridSize, GridSize];
+        for (int i = 0; i < GridSize; i++)
         {
-            for(int j = 0; j < GridSize; j++)
+            for (int j = 0; j < GridSize; j++)
             {
                 SpawnTile(TilePrefab, i, j, GridOffset);
             }
         }
+        List<GameObject> tiles = TilesGrid.Cast<GameObject>().ToList();
+        Debug.Log(tiles.Count);
+        
     }
 
-    private void SpawnTile(GameObject tile_prefab, int pos_x, int pos_z, float grid_offset)
-    {
-        Vector3 pos = new Vector3 (pos_x - grid_offset,0,pos_z - grid_offset);
-        UnityEngine.Quaternion rot = new UnityEngine.Quaternion(0f,0f,0f,0f);
-        GameObject newTile = Instantiate(tile_prefab, pos, rot);
-        newTile.GetComponentInChildren<TextMeshPro>().text = Mathf.Ceil(P[pos_x,pos_z]*10).ToString();
+    private void SpawnTile(GameObject tile_prefab, int pos_x, int pos_z, float grid_offset) {
+        Vector3 pos = new Vector3(pos_x - grid_offset, 0, pos_z - grid_offset);
+        Quaternion rot = new Quaternion(0f, 0f, 0f, 0f);
+        TilesGrid[pos_x, pos_z] = Instantiate(tile_prefab, pos, rot);
+        GameObject newTile = TilesGrid[pos_x, pos_z];
+        newTile.GetComponentInChildren<TextMeshPro>().text = Mathf.Ceil(P[pos_x, pos_z] * 10).ToString();
         //TilesGrid[pos_x,pos_z] = newTile;
-        newTile.GetComponent<Renderer>().material.SetColor("_Color", new Color(1-P[pos_x,pos_z],0f,0f));
+        newTile.GetComponent<Renderer>().material.SetColor("_Color", new Color(1 - P[pos_x, pos_z], 0f, 0f));
         //if(pos_x == GridSize -1 && pos_z == GridSize -1){newTile.GetComponent<Renderer>().material.SetColor("_Color", new Color(0f,0f,0f));}
     }
 
-    private float[,] GeneratePerlinGrid(int size)
-    {
-        int PerlinOffset = (int)Mathf.Floor(Random.Range(0f,(float)PerlinRange));
-        P = new float[size,size];
-        for(int i = 0; i < size; i++)
+    private float[,] GeneratePerlinGrid(int size) {
+        int PerlinOffset = (int)Mathf.Floor(Random.Range(0f, (float)PerlinRange));
+        P = new float[size, size];
+        for (int i = 0; i < size; i++)
         {
-            for(int j = 0; j < size; j++)
+            for (int j = 0; j < size; j++)
             {
-                P[i,j] = Mathf.PerlinNoise(PerlinOffset + PerlinScale*i*0.99f,PerlinOffset + PerlinScale*j*0.99f);
+                P[i, j] = Mathf.PerlinNoise(PerlinOffset + PerlinScale * i * 0.99f, PerlinOffset + PerlinScale * j * 0.99f);
             }
         }
         return P;
